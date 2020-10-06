5ceac622f949a2020698f04899015c66becea5fa
joshdgeek <mhiztaaustin@gmail.com>
2020-10-06 11:42:48 +0100

    fixed ui rendering

---
+++ prs.js
@@ -0,0 +1,90 @@
+let arrs = []
+function gg(a,b,arr){
+        let one = arr[0];
+        let two = arr[1];
+        var add = two-one;
+        var mult = two/one;
+
+    if(arr.length === 1){
+        return null;
+    }
+    else{
+         arr.splice(0,1)
+        let notification =    "new array: " + arr  + "<br/>" + "test cases: "+arr[0] + "," + arr[1];
+     
+        if(arr[0]+add===arr[1]){
+
+                if(arr[0]>arr[1]){
+                    return ("<br/>"+(notification + "<br/>"+"subtraction pattern, value: "+ add)+"<br/>"+gg(a,b,arr)+"<br/>")
+                } 
+                else if(arr[0]<arr[1]){
+                    return ("<br/>"+ (notification + "<br/>"+"additional pattern,value: "+ add)+"<br/>"+gg(a,b,arr)+"<br/>")
+                }
+
+                return gg(a,b,arr)
+        }//addiition and subtraction verification
+
+        else if(arr[0]*mult===arr[1]){
+            if(arr[0]>arr[1]){
+                console.log(notification + "<br/>"+"division pattern observed ")+gg(a,b,arr)+"<br/>"
+            } 
+            else if(arr[0]<arr[1]){
+              return "<br/>"+(notification + "<br/>"+"multiplication pattern, value: "+ mult)+"<br/>"+gg(a,b,arr)+"<br/>"
+            }
+            return gg(a,b,arr)
+        }
+
+        else if(arr[0]===Math.sqrt(arr[1])){
+                if(arr[0]<arr[1]){
+                return "<br/>"+(notification + "<br/>"+"power series pattern observed" + "<br/>") + gg(a,b,arr)+ "<br/>"
+                
+                }
+            return gg(a,b,arr)
+        }
+
+         else{
+            return "<br/>"+(notification + "<br/>"+"no more pattern observed: program terminated !")
+        }
+       
+    }// multiplication and division pattern 
+}   
+
+ 
+
+  function submit(){
+    let numbers = document.querySelector(".numbers");
+
+      var x = prompt("your number :");
+      var y = parseInt(x)
+      if(Number.isInteger(y)){
+          arrs.push(y)
+      }
+      console.log(arrs)
+      numbers.innerHTML = arrs
+  }
+ 
+  function activate(){
+   let result = document.querySelector(".result")   
+  
+   let answer = gg(arrs[0],arrs[1],arrs)
+    console.log(gg(arrs[0],arrs[1],arrs));
+   result.innerHTML = answer
+  }
+
+ function edit(){
+    let numbers = document.querySelector(".numbers");
+      arrs.pop()
+      console.log(arrs)
+      numbers.innerHTML = arrs
+  }
+
+
+  
+
+
+
+
+//
+
+//establish variables
+//perform a recursive algorithm containing conditional statement to verify pattern
